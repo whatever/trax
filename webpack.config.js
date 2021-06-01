e988bbdb6afdc4a3017f305223c22fa76da26cee
Matt 3 <not.mattowen@gmail.com>
2021-05-31 21:27:07 -0400

    :wolf: ayyy builds into "docs" for now

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname + "/src",
   entry: './index.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/docs',
     filename: 'svv.im.bundled.min.js',
     libraryTarget: 'var',
     library: 'svv',
