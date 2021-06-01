43ef04445b505daebd9f184aa2eeaa0e510d8586
Matt 3 <not.mattowen@gmail.com>
2021-05-31 22:40:03 -0400

    :100: env it up on the cubemap

--- src/index.js
+++ src/index.js
@@ -39,6 +39,17 @@ export class App extends Basic3 {
   }
 
   generateLogo() {
+
+    let logoRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
+      format: THREE.RGBFormat,
+      generateMipmaps: true,
+      minFilter: THREE.LinearMipmapLinearFilter,
+      encoding: THREE.sRGBEncoding,
+    });
+
+    let logoCubeCamera = new THREE.CubeCamera(1, 10000, logoRenderTarget);
+    this.logoCubeCamera  = logoCubeCamera;
+
     let scene = this.scene;
     let loader = new THREE.FontLoader();
     loader.load("helvetiker.json", function (font) {
@@ -47,22 +58,25 @@ export class App extends Basic3 {
         size: 1,
         height: 1,
       });
-      let mat = new THREE.MeshPhysicalMaterial({
-        "color": 0x33303A,
-        "emissive": 0x000000,
-        "roughness": 0.25,
-        "reflectivity": 1.0,
-        "clearcoat": 1.0,
+      let mat = new THREE.MeshBasicMaterial({
+        envMap: logoRenderTarget.texture,
+        combine: THREE.MultiplyOperation,
+        reflectivity: 0.9,
       });
       let mesh = new THREE.Mesh(geo, mat);
       mesh.position.set(10, 5, -2);
+      logoCubeCamera.position.set(10, 5, -2);
       mesh.lookAt(-0.2, 10, 0.3);
+      this.logo = mesh;
 
       scene.add(mesh);
-    });
+    }.bind(this));
   }
 
   generateCity() {
+
+    console.log(generateTexture(this.renderer));
+
     for (let i = 0; i <= 20; i++) {
       for (let j = -10; j <= 10; j++) {
         let w = 1;
@@ -92,9 +106,56 @@ export class App extends Basic3 {
     this.camera.position.y = this.center.y;
     this.camera.position.z = this.center.z;
     this.camera.lookAt(this.target);
+
+    if (this.logo) {
+      let t = +new Date() / 1000.0;
+      this.logo.rotation.x = t;
+    }
   }
 
   draw() {
     this.renderer.render(this.scene, this.camera);
+    this.logoCubeCamera.update(this.renderer, this.scene);
   }
+}
+
+
+function generateTexture(renderer) {
+  // build a small canvas 32x64 and paint it in white
+  var canvas = document.createElement('canvas');
+  canvas.width = 32;
+  canvas.height = 64;
+  var context = canvas.getContext('2d');
+  // plain it in white
+  context.fillStyle = '#ffffff';
+  context.fillRect(0, 0, 32, 64);
+  // draw the window rows - with a small noise to simulate light variations in each room
+  for (var y = 2; y < 64; y += 2) {
+    for (var x = 0; x < 32; x += 2) {
+      var value = Math.floor(Math.random() * 64);
+      context.fillStyle = 'rgb(' + [value, value, value].join(',') + ')';
+      context.fillRect(x, y, 2, 1);
+    }
+  }
+
+  // build a bigger canvas and copy the small one in it
+  // This is a trick to upscale the texture without filtering
+  var canvas2 = document.createElement('canvas');
+  canvas2.width = 512;
+  canvas2.height = 1024;
+  var context = canvas2.getContext('2d');
+  // disable smoothing
+  context.imageSmoothingEnabled = false;
+  context.webkitImageSmoothingEnabled = false;
+  context.imageSmoothingEnabled = false;
+  // then draw the image
+  context.drawImage(canvas, 0, 0, canvas2.width, canvas2.height);
+  // return the just built canvas2
+
+
+  var texture = new THREE.Texture(canvas2);
+  texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+  texture.needsUpdate = true;
+
+  return texture;
 }
